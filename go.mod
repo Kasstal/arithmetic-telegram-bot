155a9ca9d0a1a5381f41b5de1db7a350f00859af
Kasstal <kassymkhan2503@gmail.com>
2025-06-03 22:02:27 +0500

    goevaluate

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module arithmetic-telegram-bot
+
+go 1.23.6
+
+require github.com/Knetic/govaluate v3.0.0+incompatible
--- internal/infrastructure/evaluator/goevaluate.go
+++ internal/infrastructure/evaluator/goevaluate.go
@@ -1,1 +1,56 @@
 package evaluator
+
+import (
+	"arithmetic-telegram-bot/internal/domain"
+	"github.com/Knetic/govaluate"
+	"strings"
+)
+
+type GoEvaluator struct{}
+
+func (e *GoEvaluator) Evaluate(expr domain.Expression) (domain.Answer, error) {
+	expression, err := govaluate.NewEvaluableExpression(string(expr))
+	if err != nil {
+		errmsg := err.Error()
+		if strings.Contains(errmsg, "syntax error") {
+			return 0, &domain.CalculatorError{
+				Type:    domain.ErrorTypeInvalidExpression,
+				Message: "Invalid expression syntax",
+				Err:     err,
+			}
+		}
+	}
+		return 0, &domain.CalculatorError{
+			Type:    domain.ErrorTypeUnknown,
+			Message: "Unknown error during expression evaluation",
+			Err:     err,
+		}
+	}
+
+	result, err := expression.Evaluate(nil)
+	if err != nil {
+		errMsg := err.Error()
+		if strings.Contains(errMsg, "division by zero") {
+			return 0, &domain.CalculatorError{
+				Type:    domain.ErrorTypeDivisionByZero,
+				Message: "Division by zero error",
+				Err:     err,
+			}
+		}
+		return 0, &domain.CalculatorError{
+			Type:    domain.ErrorTypeUnknown,
+			Message: "Unknown error during expression evaluation",
+			Err:     err,
+		}
+
+		if floatResult, ok := result.(float64); ok {
+			return domain.Answer(floatResult), nil
+		}
+
+		return 0, &domain.CalculatorError{
+			Type:    domain.ErrorTypeUnknown,
+			Message: "Unexpected result type",
+			Err:     nil,
+	}
+}
+
